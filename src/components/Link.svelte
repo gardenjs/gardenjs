01c3c60b59ea2b02abe2400c592b6e87fa9a640a
robindanzinger <3295716+robindanzinger@users.noreply.github.com>
2020-10-14 01:15:26 +0200

    add event modifier in svelte way

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -2,11 +2,11 @@
   export let href
 
   function handleClick(evt) {
-    evt.preventDefault()
     globalThis.history.pushState({}, '', href)
   }
+
 </script>
-<a href={href} on:click={handleClick}>
+<a href={href} on:click|preventDefault={handleClick}>
 <slot />
 </a>
 
