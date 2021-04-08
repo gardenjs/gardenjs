a63c9b9f46c7478e7b84e0451e10b872c6749b9b
robindanzinger <3295716+robindanzinger@users.noreply.github.com>
2021-04-08 15:59:25 +0200

    add vite

--- src/server.js
+++ src/server.js
@@ -1,39 +1,57 @@
 import express from 'express'
-import {initReloadServer} from './reloadserver.js'
+// import {initReloadServer} from './reloadserver.js'
 import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
 import { config } from './config.js'
+// import { createServer as createViteServer } from 'vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const app = express()
 
 const port = config.serverport
 
-app.use(express.static(__dirname + '/../public'))
-app.use(express.static(__dirname + '/assets'))
-app.use(express.static('src/assets/'))
-app.use(express.static(config.destination))
+async function createServer() {
+  const app = express()
 
-app.get("/", componentRoute)
-app.get("/garden", componentRoute)
-app.get("/garden/*", componentRoute)
+//  app.use(express.static(__dirname + '/../public'))
+//  app.use(express.static(__dirname + '/assets'))
+//  app.use(express.static('src/assets/'))
+//  app.use(express.static(config.destination))
 
-function componentRoute(req, res, next) {
-  const filepath = path.resolve(__dirname, '../public/gardenapp.html')
-  const html = fs.readFileSync(filepath, 'utf-8')
-  res.send(html)
+  // Create vite server in middleware mode. This disables Vite's own HTML
+  // serving logic and let the parent server take control.
+//  const vite = await createViteServer({
+//    server: { middlewareMode: true },
+//    configFile: false,
+//    root: '../public/gardenapp.html'
+//  })
+//  // use vite's connect instance as middleware
+//  app.use(vite.middlewares)
+//
+
+  app.get("/", componentRoute)
+  app.get("/garden", componentRoute)
+  app.get("/garden/*", componentRoute)
+
+  function componentRoute(req, res, next) {
+    const filepath = path.resolve(__dirname, '../public/gardenapp.html')
+    const html = fs.readFileSync(filepath, 'utf-8')
+    res.send(html)
+  //  res.send(vite.transformIndexHtml(req.url, html))
+  }
+
+  app.get("/gardenframe", componentframeRoute)
+
+  function componentframeRoute(req, res, next) {
+    const filepath = path.resolve(__dirname, '../public/gardenframe.html')
+    const html = fs.readFileSync(filepath, 'utf-8')
+    res.send(html)
+  //  res.send(vite.transformIndexHtml(req.url, html))
+  }
+
+  app.listen(port, () => console.log(`Listening to port ${port}`))
 }
 
-app.get("/gardenframe", componentframeRoute)
-
-function componentframeRoute(req, res, next) {
-  const filepath = path.resolve(__dirname, '../public/gardenframe.html')
-  const html = fs.readFileSync(filepath, 'utf-8')
-  res.send(html)
-}
-
-initReloadServer()
-app.listen(port, () => console.log(`Listening to port ${port}`))
+createServer()
