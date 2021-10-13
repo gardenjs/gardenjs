5945eab4a55eba9254f207ee92b31839eb9dc1e8
Martin Farkas <info@martinfarkas.com>
2021-10-13 18:08:41 +0200

    move css to scss file

--- src/components/InputSelectionComponent.svelte
+++ src/components/InputSelectionComponent.svelte
@@ -17,20 +17,3 @@ const handleselect = item => () => {
   <li class:active="{selected == item}" on:click={handleselect(item)}>{item}</li>
   {/each}
 </ul>
-
-<style>
-ul {
-  list-style: none;
-  padding: 0;
-}
- li {
-  border: 1px solid transparent;
-   padding: 5px 12px;
-   cursor: pointer;
- }
- .active {
-   border: 1px solid green;
-   color: green;
-   font-weight: bold; 
- }
-</style>
