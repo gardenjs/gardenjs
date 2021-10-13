1cf4c7e3fc654832ccd125bedf67fdc6bf080d37
Martin Farkas <info@martinfarkas.com>
2021-10-13 10:09:43 +0200

    change nav icon

--- src/components/topbar/Topbar.svelte
+++ src/components/topbar/Topbar.svelte
@@ -1,9 +1,9 @@
 <div class="topbar">
-  <div class="btn-burger">
-    <div class="toggleIcon" bind:this={toggleIcon} on:click={toggle}>
-      <div class="bar1"></div>
-      <div class="bar2"></div>
-      <div class="bar3"></div>
+  <div class="topbar__inner">
+    <div class="topbar__nav-icon">
+      <div bind:this={toggleIcon} on:click={toggle}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="34" viewBox="0 0 24 24" width="34" fill="#fff"><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+      </div>
     </div>
   </div>
 </div>
