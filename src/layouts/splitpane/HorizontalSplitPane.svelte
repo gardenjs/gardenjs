152e225c3d21897a81f177311c7363652bb0bf0d
Martin Farkas <info@martinfarkas.com>
2021-10-13 18:09:08 +0200

    move icon from css to inline

--- src/layouts/splitpane/HorizontalSplitPane.svelte
+++ src/layouts/splitpane/HorizontalSplitPane.svelte
@@ -22,7 +22,9 @@
 
 <div class="full flex-column" bind:this={element} >
   <div class="flexfix" style="height: {topheight}"><slot name="top" /></div>
-  <div class="flexfix dragbar-horizontal" on:mousedown={register}></div>
+  <div class="flexfix dragbar-horizontal" on:mousedown={register}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3.984 15v-2.016h16.031V15H3.984zm16.032-6v2.016H3.985V9h16.031z"/></svg>
+  </div>
   <div class="full flex-grow"><slot name="bottom" /></div>
 </div>
 
--- src/layouts/splitpane/VerticalSplitPane.svelte
+++ src/layouts/splitpane/VerticalSplitPane.svelte
@@ -23,7 +23,8 @@
 </script>
 <div class="flex-row" bind:this={element} >
   <div style="width: {leftwidth}"><slot name="left" /></div>
-  <div class="dragbar-vertical" on:mousedown={register}></div>
+  <div class="dragbar-vertical" on:mousedown={register}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"><path d="M3.75 3.333v17.334h2.167V3.333H3.75zm6.5 17.334V3.333H8.083v17.334h2.167z"/></svg>
+  </div>
   <div style="width: calc(100% - {leftPlusDragbarWidth}px)"><slot name="right" /></div>
 </div>
-
