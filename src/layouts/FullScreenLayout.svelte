526791d34385f4cd15bfbd16c48eaffb12234e82
Martin Farkas <info@martinfarkas.com>
2021-10-14 09:57:30 +0200

    move styles to layout.scss

--- src/layouts/FullScreenLayout.svelte
+++ src/layouts/FullScreenLayout.svelte
@@ -1,13 +1,3 @@
 <div class="fullscreen">
   <slot/>
 </div>
-<style>
-.fullscreen {
-  margin: 0;
-  padding: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  overflow: hidden;
-}
-</style>
