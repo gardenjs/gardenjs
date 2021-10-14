1cb39a1dfb6ad427973832ac6d82ffe6287cd322
Martin Farkas <info@martinfarkas.com>
2021-10-14 10:23:19 +0200

    changed icons

--- src/layouts/splitpane/VerticalSplitPane.svelte
+++ src/layouts/splitpane/VerticalSplitPane.svelte
@@ -24,7 +24,7 @@
 <div class="flex-row" bind:this={element} >
   <div style="width: {leftwidth}"><slot name="left" /></div>
   <div class="dragbar-vertical" on:mousedown={register}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="24"><path d="M3.75 3.333v17.334h2.167V3.333H3.75zm6.5 17.334V3.333H8.083v17.334h2.167z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="14" fill="#currentColor"><path d="M3.75 3.333v17.334h2.167V3.333H3.75zm6.5 17.334V3.333H8.083v17.334h2.167z"/></svg>
   </div>
   <div style="width: calc(100% - {leftPlusDragbarWidth}px)"><slot name="right" /></div>
 </div>
