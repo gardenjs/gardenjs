4898196121ed49cfbc207893371fcf2cbea822ab
robindanzinger <3295716+robindanzinger@users.noreply.github.com>
2022-02-16 20:10:25 +0100

    add button status active class

--- src/components/ComponentPage.svelte
+++ src/components/ComponentPage.svelte
@@ -63,23 +63,28 @@ function tabselectionchange(evt) {
   selectedtab = evt.detail.selecteditem
 }
 
-let frameheight = "100%"
-let framewidth = "100%"
+let frameheight = '100%'
+let framewidth = '100%'
+let framesize = 'full'
 function setFramesizeMobile() {
-  frameheight = "1170px"
-  framewidth = "550px"
+  frameheight = '1170px'
+  framewidth = '550px'
+  framesize = 'small'
 }
 function setFramesizeFullTablet() {
-  frameheight = "1080px"
-  framewidth = "810px"
+  frameheight = '1080px'
+  framewidth = '810px'
+  framesize = 'medium'
 }
 function setFramesizeFullDesktop() {
-  frameheight = "960px"
-  framewidth = "1536px"
+  frameheight = '960px'
+  framewidth = '1536px'
+  framesize = 'large'
 }
 function setFramesizeFull() {
-  frameheight = "100%"
-  framewidth = "100%"
+  frameheight = '100%'
+  framewidth = '100%'
+  framesize = 'full'
 }
 let landscape = false 
 function toggleOrientation() {
@@ -87,12 +92,11 @@ function toggleOrientation() {
 }
 let style = ""
 $: {
-  if (landscape)
+  if (landscape) {
     style = `width: ${frameheight}; height: ${framewidth};`
-  else 
+  } else {
     style = `width: ${framewidth}; height: ${frameheight};`
-  
-  console.log('style', style)
+  }
 }
 
 </script>
@@ -103,16 +107,16 @@ $: {
   <div class="actionbar">
     <h1 class="title__h1">{das.name}</h1>
     <div class="framesize-nav">
-      <button on:click={setFramesizeMobile} rel="Small" class="tooltip">
+      <button on:click={setFramesizeMobile} rel="Small" class="tooltip" class:active={framesize === 'small'} >
         <svg xmlns="http://www.w3.org/2000/svg" class:landscape={landscape} height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M15.5 1h-8A2.5 2.5 0 005 3.5v17A2.5 2.5 0 007.5 23h8a2.5 2.5 0 002.5-2.5v-17A2.5 2.5 0 0015.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/></svg>
       </button>
-      <button on:click={setFramesizeFullTablet} rel="Medium" class="tooltip">
+      <button on:click={setFramesizeFullTablet} rel="Medium" class="tooltip" class:active={framesize === 'medium'} >
         <svg xmlns="http://www.w3.org/2000/svg" class:landscape={landscape} height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M18.5 0h-14A2.5 2.5 0 002 2.5v19A2.5 2.5 0 004.5 24h14a2.5 2.5 0 002.5-2.5v-19A2.5 2.5 0 0018.5 0zm-7 23c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm7.5-4H4V3h15v16z"/></svg>
       </button>
-      <button on:click={setFramesizeFullDesktop} rel="Large" class="tooltip">
+      <button on:click={setFramesizeFullDesktop} rel="Large" class="tooltip" class:active={framesize === 'large'} >
         <svg xmlns="http://www.w3.org/2000/svg" class:landscape={landscape} height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M20 18c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H0v2h24v-2h-4zM4 6h16v10H4V6z"/></svg>
       </button>
-      <button on:click={setFramesizeFull} rel="Full" class="tooltip">
+      <button on:click={setFramesizeFull} rel="Full" class="tooltip" class:active={framesize === 'full'} >
         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M21 2H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h7l-2 3v1h8v-1l-2-3h7c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 12H3V4h18v10z"/></svg>
       </button>
       <button on:click={toggleOrientation} rel="Toggle Portrait/Landscape" class="tooltip">
