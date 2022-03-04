3d37af4fa2ee44350190e741f667544e0f91d422
onza <info@martinfarkas.com>
2022-03-04 17:20:52 +0100

    move infopane/tabs scss to comp & change scss vars to css vars

--- src/components/tabs/TabsComponent.svelte
+++ src/components/tabs/TabsComponent.svelte
@@ -21,7 +21,7 @@ const dispatch = createEventDispatcher()
  
 </script>
 
-<div class="infopanel__nav">
+<div class="tabs__nav">
   <nav>
     <ul>
       {#each tabs as tab, index}
@@ -30,10 +30,59 @@ const dispatch = createEventDispatcher()
     </ul>
   </nav>
 </div>
-<div class="infopanel__pane">
+<div class="tabs__pane">
   {#if selected && selected.page }
   <TabContent item={selected} on:out={handleout} />
   {:else}
   <slot>No tab content provided</slot>
   {/if}
 </div>
+
+<style>
+.tabs__nav nav {
+  background-color: var(--infopanel-nav-bg-color);
+}
+.tabs__nav nav ul {
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: row;
+  overflow-x: auto;
+  height: 2rem;
+  margin: 0;
+  padding: 0;
+}
+.tabs__nav nav li {
+  display: flex;
+  flex: auto;
+  height: 100%;
+}
+.tabs__nav nav li button {
+  width: 100%;
+  min-width: 150px;
+  height: 100%;
+  font-size: 0.9rem;
+  color: var(--infopanel-nav-color);
+  text-align: center;
+  cursor: pointer;
+  border: none;
+  background: none;
+  border-bottom: 3px solid var(--infopanel-nav-border-color);
+}
+.tabs__nav nav li button:hover {
+  color: var(--infopanel-nav-hover-color);
+  border-bottom: 3px solid var(--infopanel-nav-border-hover-color);
+}
+.tabs__nav nav li button.active {
+  color: var(--infopanel-nav-hover-color);
+  border-bottom: 3px solid var(--infopanel-nav-hover-color);
+}
+.tabs__nav nav li button.active:hover {
+  border-bottom: 3px solid var(--infopanel-nav-border-act-hover-color);
+}
+
+.tabs__pane {
+  padding: 20px;
+  background-color: var(--infopanel-pane-bg-color);
+  color: var(--infopanel-pane-text-color);
+}
+</style>
