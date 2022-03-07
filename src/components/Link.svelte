2a4ba647fe88d7cfd0693d40beb9195f4f3ea3ed
onza <info@martinfarkas.com>
2022-03-07 15:17:40 +0100

    finetuning

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -15,12 +15,12 @@
   a {
     display: block;
     margin: -0.25rem -1rem -1rem;
-    padding: 0.125rem 1rem;
+    padding: 0.25rem 1rem;
     text-transform: initial;
     text-decoration: none;
     font-weight: 400;
     color: var(--gray-900);
-    border-radius: 0.25rem;
+    border-radius: 0.5rem;
   }
   a:hover,
   a:focus {
