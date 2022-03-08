de109424f4e7f340af0ac3ab05530505f8a30958
onza <info@martinfarkas.com>
2022-03-08 10:48:57 +0100

    color finetuning

--- src/components/InputSelectionComponent.svelte
+++ src/components/InputSelectionComponent.svelte
@@ -28,16 +28,16 @@ const handleselect = item => () => {
   margin: 0.125rem 0;
   padding: 0.5rem 0.75rem;
   font-size: 0.9rem;
+  color: var(--gray-900);
   cursor: pointer;
 }
 .component__stories li:nth-child(odd) {
   background-color: var(--gray-100);
-  color: var(--gray);
 }
 .component__stories li:hover,
 .component__stories li:focus,
 .component__stories li.active {
-    color: var(--gray-50);
+    color: var(--white);
     background-color: var(--gray-600);
   }
 </style>
