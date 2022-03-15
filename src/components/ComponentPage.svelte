341b2faa64e0c3ab6361ffe07c97af3145b14cab
onza <info@martinfarkas.com>
2022-03-15 09:18:10 +0100

    remove h1, add transition

--- src/components/ComponentPage.svelte
+++ src/components/ComponentPage.svelte
@@ -93,9 +93,9 @@ function toggleOrientation() {
 let style = ""
 $: {
   if (landscape) {
-    style = `width: ${frameheight}; height: ${framewidth};`
+    style = `width: ${frameheight}; height: ${framewidth}; transition: 0.4s;`
   } else {
-    style = `width: ${framewidth}; height: ${frameheight};`
+    style = `width: ${framewidth}; height: ${frameheight}; transition: 0.4s;`
   }
 }
 
@@ -105,7 +105,6 @@ $: {
 <HorizontalSplitPane topheight='65vh'>
 <div slot="top" class="is-full is-flex-column">
   <div class="actionbar">
-    <h1 class="h1">{das.name}</h1>
     <div class="framesize-nav">
       <button on:click={setFramesizeMobile} rel="Small" class="tooltip" class:active={framesize === 'small'} >
         <svg xmlns="http://www.w3.org/2000/svg" class:landscape={landscape} height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M15.5 1h-8A2.5 2.5 0 005 3.5v17A2.5 2.5 0 007.5 23h8a2.5 2.5 0 002.5-2.5v-17A2.5 2.5 0 0015.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/></svg>
