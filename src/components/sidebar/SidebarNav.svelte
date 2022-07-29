53ef5cbac75b3f44b096890dfe72eb5808f28557
onza <info@martinfarkas.com>
2022-07-29 18:02:18 +0200

    add button with icon to fold/unfold nav folders

--- src/components/sidebar/SidebarNav.svelte
+++ src/components/sidebar/SidebarNav.svelte
@@ -19,7 +19,9 @@
       {:else}
         <span class="unit">
           <li class="folder">
-            <span class="folder-label">{key}</span>
+            <button class="folder-btn" title="fold/unfold">
+              <span class="folder-label">{key}</span><span class="folder-icon"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M9.4 18L8 16.6l4.6-4.6L8 7.4 9.4 6l6 6z"/></svg></span>
+            </button>
             <svelte:self node={node[key]} selectedNode={selectedNode} />
           </li>
         </span>
@@ -37,7 +39,7 @@ ul li {
   list-style: none;
   margin: 0;
   color: var(--c-basic-900);
-  font-size: 0.813rem;
+  font-size: 0.938rem;
   font-weight: 600;
   text-transform: uppercase;
 }
@@ -46,10 +48,23 @@ ul li {
   margin: 0.5rem 0 1.5rem 1rem;
 }
 .folder {
-  margin: 0.5rem 0 1rem;
+  margin: 0.5rem 0 0;
+}
+.folder-btn {
+  display: flex;
+  background: transparent;
+  cursor: pointer;
+  height: 24px;
 }
 .folder-label {
-  display: block;
-  padding: 0 0 0.5rem;
+  display: flex;
+  align-self: center;
+}
+.folder-icon {
+  margin: 0 0 0 0.5rem;
+}
+.folder-btn:hover > .folder-icon {
+  transform: rotate(90deg);
+  transition: 0.2s ease-in-out;
 }
 </style>
