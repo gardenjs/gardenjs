aca364a294fef432a9632efb9175126a27f15959
onza <info@martinfarkas.com>
2022-09-12 14:22:44 +0200

    changed icon

--- src/components/sidebar/SidebarNav.svelte
+++ src/components/sidebar/SidebarNav.svelte
@@ -9,25 +9,25 @@
   }
 
 </script>
-  <ul>
-    {#each nodes as node }
-      {#if node.isLeaf}
-        <li>
-          <Link href={node.href} selected="{node.selected}">{node.name}</Link>
-        </li>
-      {:else}
-        <li class="folder">
-          <button class="folder-button" title="fold/unfold" on:click={() => toggleFolderFoldStatus(node)}>
-            <span class="folder-label">{node.name}</span>
-            <span class="folder-icon" class:unfolded={node.unfolded}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M10 17V7l5 5z"/></svg></span>
-          </button>
-          {#if node.unfolded}
-            <svelte:self nodes={node.children} on:out />
-          {/if}
-        </li>
-      {/if}
-    {/each}
-  </ul>
+<ul>
+  {#each nodes as node }
+    {#if node.isLeaf}
+      <li>
+        <Link href={node.href} selected="{node.selected}">{node.name}</Link>
+      </li>
+    {:else}
+      <li class="folder">
+        <button class="folder-button" title="fold/unfold" on:click={() => toggleFolderFoldStatus(node)}>
+          <span class="folder-label">{node.name}</span>
+          <span class="folder-icon" class:unfolded={node.unfolded}><svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="20"><path d="M7.4 15.375l-1.4-1.4 6-6 6 6-1.4 1.4-4.6-4.6z"/></svg>
+        </button>
+        {#if node.unfolded}
+          <svelte:self nodes={node.children} on:out />
+        {/if}
+      </li>
+    {/if}
+  {/each}
+</ul>
 
 <style>
 .folder {
@@ -62,6 +62,6 @@
   fill: var(--c-basic-700);
 }
 .folder-icon.unfolded {
-  transform: rotate(90deg);
+  transform: rotate(180deg);
 }
 </style>
