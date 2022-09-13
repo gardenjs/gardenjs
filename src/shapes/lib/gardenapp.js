baad5b8d3e7f88e4501c4cf4739a25bce4791424
onza <info@martinfarkas.com>
2022-09-13 17:02:14 +0200

    undo rename changes

--- src/shapes/lib/gardenapp.js
+++ src/shapes/lib/gardenapp.js
@@ -1,7 +1,7 @@
 import {GardenApp} from 'garden'
 import {navtree, routes} from '../base.js'
 import {dasmap} from '../importmap.js'
-import 'garden/src/assets/scss/index.scss'
+import 'garden/src/assets/scss/main.scss'
 
 const app = new GardenApp({
   target: document.body,
