c61df55ad37c1695e60512d27d7cdad4adee3c2a
onza <info@martinfarkas.com>
2022-09-16 21:02:55 +0200

    show already implemetedpixel values

--- src/components/topbar/Topbar.svelte
+++ src/components/topbar/Topbar.svelte
@@ -55,7 +55,7 @@
     </button>
     <div class="topbar__nav">
       <!-- future feature: show pixels value of stage. (styles below) -->
-      <!-- <div class="stagesize__value">1920px <span class="multiplication_sign">&#10005;</span> 986px</div> -->
+      <div class="stagesize__value">1920px <span class="multiplication_sign">&#10005;</span> 986px</div>
       <div class="stagesize-nav">
         <button rel="Small" class="tooltip" class:active={stageSize === 'small'} on:click={() => setFramesize('small')}>
           <svg xmlns="http://www.w3.org/2000/svg" class:landscape={landscape} height="24" viewBox="0 0 24 24" width="24" fill="#currentColor"><path d="M15.5 1h-8A2.5 2.5 0 005 3.5v17A2.5 2.5 0 007.5 23h8a2.5 2.5 0 002.5-2.5v-17A2.5 2.5 0 0015.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z"/></svg>
@@ -225,14 +225,14 @@
   }
  
   /* futur feature */
-  /* .stagesize__value {
+  .stagesize__value {
     font-size: 0.813rem;
     padding: 0 1.25rem;
     color: var(--c-basic-500);
   }
   .stagesize__value .multiplication_sign {
     font-size: 0.563rem;
-  } */
+  }
 
    /* stagesize */
   .stagesize-nav {
