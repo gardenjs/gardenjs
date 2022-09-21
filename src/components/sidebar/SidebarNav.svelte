81f47e9a5a5f1c690f860ead9b462fb787f437c8
onza <info@martinfarkas.com>
2022-09-21 15:23:07 +0200

    move nav level to btns move styles inside comp.

--- src/components/sidebar/SidebarNav.svelte
+++ src/components/sidebar/SidebarNav.svelte
@@ -3,13 +3,13 @@
   import { createEventDispatcher } from 'svelte'
   const dispatch = createEventDispatcher()
   export let nodes = []
-
+  export let level = 1
   function toggleFolderFoldStatus(node) {
     dispatch('out', {toggleFolderFoldStatus: {...node}})
   }
 
 </script>
-<ul class="sidebarnav">
+<ul>
   {#each nodes as node }
     {#if node.isLeaf}
       <li>
@@ -17,12 +17,12 @@
       </li>
     {:else}
       <li class="folder" class:unfolded={node.unfolded}>
-        <button class="folder_button" title="fold/unfold" on:click={() => toggleFolderFoldStatus(node)}>
+        <button class="folder_button level-{level}" title="fold/unfold" on:click={() => toggleFolderFoldStatus(node)}>
           <span class="folder_icon" class:unfolded_icon={node.unfolded}><svg height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M6.167 13.084L5 11.917l5-5 5 5-1.167 1.167L10 9.25l-3.833 3.834z"/></svg></span>
           <span class="folder_label">{node.name}</span>
         </button>
         {#if node.unfolded}
-          <svelte:self nodes={node.children} on:out />
+          <svelte:self nodes={node.children} level={level + 1} on:out />
         {/if}
       </li>
     {/if}
@@ -41,7 +41,7 @@
   border: none;
 }
 .folder.unfolded {
-  background-color: var(--c-basic-150);
+  background-color: var(--c-basic-100);
 }
 .folder_button {
   display: flex;
@@ -49,21 +49,19 @@
   align-items: center;
   width: 100%;
   margin: 0;
-  padding: 0.5rem 0.5rem 0.5rem 0.75rem;
+  padding: 0.5rem 0.25rem 0.5rem 0.5rem ;
   text-align: left;
-  background: transparent;
   border-left: 3px solid transparent;
   cursor: pointer;
 }
 .folder_button:hover {
   border-left: 3px solid var(--c-basic-500);
-  background-color: var(--c-basic-150);
 }
 .folder_label {
   display: flex;
   align-self: center;
+  order: 1;
   width: 100%;
-  padding-left: 0.25rem;
   color: var(--c-basic-900);
   font-size: 0.875rem;
   font-weight: 600;
@@ -72,9 +70,12 @@
 }
 .folder_icon {
   display: flex;
+  order: 2;
   width: 1.25rem;
   height: 1.25rem;
   transition: 0.2s;
+  margin-left: 0;
+  margin-right: 0.25rem;
 }
 .folder_icon svg {
   fill: var(--c-basic-700);
@@ -82,5 +83,45 @@
 .folder_icon.unfolded_icon {
   transform: rotate(180deg);
   transition: 0.2s;
+}
+
+
+.level-1 {
+  background: transparent;
+}
+.level-1:hover {
+  background-color: var(--c-basic-150);
+}
+.level-1 .folder_icon {
+  margin-left: 0;
+  margin-right: 0.25rem;
+}
+.level-1 .folder_label {
+  order: 2;
+}
+.level-1 .folder_icon {
+  order: 1;
+}
+/* level 2 */
+.level-2 {
+  padding-left: 2.125rem;
+  background-color: var(--c-basic-100);
+}
+.unfolded .level-2 {
+  background-color: var(--c-basic-150);
+}
+.level-2:hover {
+  background-color: var(--c-basic-200);
+}
+/* level 3 */
+.level-3 {
+  padding-left: 2.75rem;
+  background-color: var(--c-basic-100);
+}
+.unfolded .level-3 {
+  background-color: var(--c-basic-200);
+}
+.level-3:hover {
+  background-color: var(--c-basic-250);
 }
 </style>
