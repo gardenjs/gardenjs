d31486eb845859fd10e04cbb628431827c6d057c
Robin Danzinger <robin.danzinger@gmx.com>
2022-11-18 17:56:32 +0100

    use css-styles from config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,6 @@
 import fs from 'fs'
 import path from 'path'
 
-// const content = fs.existsSync(path.resolve('garden.config.js')) ? fs.readFileSync(path.resolve('rabbit.config.json'), 'utf8') : '{"garden": {}}'
-
-// TODO mix default and specific values
-// export default JSON.parse(content).garden 
-
 export default async function () {
   if (fs.existsSync(path.resolve('garden.config.js'))) {
     console.log('the path is', path.resolve('garden.config.js'))
