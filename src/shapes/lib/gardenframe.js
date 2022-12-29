35f42ca90667be3e394e34ff39681da210390ccc
Robin Danzinger <robin.danzinger@gmx.com>
2022-12-29 14:12:08 +0100

    update versions

--- src/shapes/lib/gardenframe.js
+++ src/shapes/lib/gardenframe.js
@@ -1,13 +1,13 @@
 import {GardenFrame} from 'garden'
 import {routes} from '../base.js'
 import {componentmap, dasmap} from '../importmap.js'
-import config from './rabbit.config.js'
-
-if (config.additional_style_files) {
-  config.additional_style_files.forEach(style_file) {
-    await import(css_file)
-  }
-}
+// import config from './rabbit.config.js'
+// 
+// if (config.additional_style_files) {
+//   config.additional_style_files.forEach(style_file) {
+//     await import(css_file)
+//   }
+// }
 
 const app = new GardenFrame({
   target: document.body,
