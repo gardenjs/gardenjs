2ea0ff278a958c9f5dd6a38421e5f07458ad79ed
onza <info@martinfarkas.com>
2023-01-25 11:44:37 +0100

    add searchbox (only html & css)

--- src/components/sidebar/Sidebar.svelte
+++ src/components/sidebar/Sidebar.svelte
@@ -20,6 +20,12 @@
     </a>
     <nav>
       <ul>
+        <li class="search">
+          <form id="search" class="searchform" method="post" action="">
+            <input class="searchform_input" type="text" name="" value="" placeholder="search term">
+            <button id="searchform_submit" class="searchform_submit" name="search[submitButton]" title="send search request"><svg xmlns="http://www.w3.org/2000/svg" width="16" viewBox="0 0 24 24" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><path d="M21 21l-4.35-4.35"/></svg></button>
+          </form>
+        </li>
         <li>
           <button class="unfold-all_button" title="fold/unfold all" on:click={toggleRootFolders}>
             <span class="unfold-all_label">Collapse Navigation</span>
@@ -36,7 +42,7 @@
       <SidebarNav nodes={nodes} on:out />
       <ul>
         <li>
-          <a class="github" href="https://github.com/rabbitdevelopment/garden" target="_blank">
+          <a class="github" href="https://github.com/rabbitdevelopment/garden" target="_blank" rel="noreferrer">
             <svg height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg"><path d="M11.999.296C5.373.296 0 5.67 0 12.296c0 5.302 3.438 9.8 8.207 11.387.6.11.819-.26.819-.579 0-.284-.01-1.04-.017-2.04-3.337.725-4.042-1.61-4.042-1.61C4.422 18.07 3.635 17.7 3.635 17.7c-1.09-.745.082-.73.082-.73 1.205.085 1.838 1.237 1.838 1.237 1.07 1.833 2.81 1.304 3.493.997.109-.775.418-1.304.762-1.604-2.665-.303-5.467-1.333-5.467-5.931 0-1.31.468-2.381 1.236-3.22-.124-.304-.536-1.524.117-3.176 0 0 1.008-.323 3.3 1.23A11.473 11.473 0 0112 6.1a11.51 11.51 0 013.005.403c2.29-1.553 3.296-1.23 3.296-1.23.655 1.652.243 2.872.12 3.176.769.839 1.233 1.91 1.233 3.22 0 4.61-2.806 5.624-5.479 5.921.431.37.815 1.103.815 2.223 0 1.604-.015 2.898-.015 3.291 0 .321.216.695.825.578 4.765-1.59 8.2-6.085 8.2-11.385 0-6.628-5.373-12-12.001-12"/></svg>
             <span>Documentation</span>
           </a>
@@ -67,7 +73,7 @@
     flex-direction: row;
     justify-content: flex-start;
     align-items: center;
-    padding: 0.25rem 0.75rem;
+    padding: 0.25rem 0.688rem;
     width: 260px;
     height: 2.75rem;
     inline-size: 260px; 
@@ -88,11 +94,50 @@
     height: calc(100vh - 44px);
     overflow-y: auto;
   }
+  .search {
+    height: 2rem;
+    padding: 0 0 0 0.688rem;
+    border-bottom: 1px solid var(--c-basic-300);
+    overflow: hidden;
+  }
+  .searchform {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .searchform_input {
+    padding: 0.125rem;
+    width: calc(100% - 2.5rem);
+    height: 2rem;
+    border: none;
+    color: blue;
+  }
+  .searchform_input::placeholder {
+    font-size: 0.813rem;
+    color: var(--c-basic-500);
+  }
+  .searchform_input:focus {
+    outline: none;
+  }
+  .searchform_submit {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 2.5rem;
+    height: 2rem;
+    background-color: var(--c-basic-150);
+    border: none;
+    cursor: pointer;
+  }
+  .searchform_submit svg {
+    color: var(--c-primary);
+  }
   .unfold-all_button {
     display: flex;
     justify-content: flex-start;
     align-items: center;
     width: 100%;
+    height: 2rem;
     margin: 0;
     padding: 0.5rem 0.75rem 0.5rem 0.688rem;
     text-align: left;
@@ -125,7 +170,7 @@
     justify-content: center;
     align-items: center; 
     margin: 1rem 0;
-    padding: 0.5rem 0.5rem 0.5rem 1rem;
+    padding: 0.5rem 0.5rem 0.5rem 0.688rem;
   }
   a.github svg {
     display: block;
