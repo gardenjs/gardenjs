9e37621b1eede618ed37019d8b0e25d28c755d9c
onza <info@martinfarkas.com>
2023-02-03 15:31:04 +0100

    add cursor blink style to h1

--- src/shapes/Welcome.svelte
+++ src/shapes/Welcome.svelte
@@ -1,3 +1,7 @@
+<h1>Hello, Garden!<span class="cursor_blink">_</span></h1>
+<p>Woohoo, you've successfully installed the component library explorer Garden. Welcome and have fun gardening.</p>
+<p>Feel free to post bugs and suggestions on <a href="https://github.com/rabbitdevelopment/garden">Github</a>.</p>
+
 <style>
   h1,p,a {
     font-family: ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Fira Mono", "Droid Sans Mono", Courier New, "monospace";
@@ -20,9 +24,13 @@
   a:hover{
     color: hsl(168, 41%, 60%);
     border-color: hsl(168, 41%, 60%);
-  } 
+  }
+  .cursor_blink {
+    animation: blink 1s steps(5,start) infinite normal none running;
+  }
+  @keyframes blink {
+    to {
+      visibility: hidden;
+    }
+  }
 </style>
-
-<h1>Hello, Garden!</h1>
-<p>Woohoo, you've successfully installed the component library explorer Garden. Welcome and have fun gardening.</p>
-<p>Feel free to post bugs and suggestions on <a href="https://github.com/rabbitdevelopment/garden">Github</a>.</p>
