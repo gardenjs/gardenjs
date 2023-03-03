48c993d008033858f00c5ab76e5ac0d0c3ffe8eb
onza <info@martinfarkas.com>
2023-03-03 17:32:47 +0100

    add stage padding if full

--- src/stageSizes.js
+++ src/stageSizes.js
@@ -19,7 +19,10 @@ const sizes = {
 
 export function computeStageStyle({stageSize= 'full', landscape = false, stageBg = 'white'}) {
   const {frameheight, framewidth} = sizes[stageSize]
-  return landscape 
-    ? `width: ${frameheight}; height: ${framewidth}; transition: 0.2s; background: ${stageBg};`
-    : `width: ${framewidth}; height: ${frameheight}; transition: 0.2s; background: ${stageBg};`
+  const size = landscape ? `width: ${frameheight}; height: ${framewidth}` : `width: ${framewidth}; height: ${frameheight}`
+  const padding = stageSize === 'full' ? 'padding: 1rem' : 'padding: 0'
+  const background = `background: ${stageBg}`
+  const transition = 'transition: 0.2s'
+ 
+  return `${size};${padding};${background}${transition};`
 }
