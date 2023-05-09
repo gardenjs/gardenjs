5dcffd9746aba18edde807c8dd9db74d56ad1361
onza <info@martinfarkas.com>
2023-05-09 11:54:47 +0200

    remove unused code

--- src/client/components/splitpanes/HorizontalSplitPane.svelte
+++ src/client/components/splitpanes/HorizontalSplitPane.svelte
@@ -25,9 +25,7 @@
 
 <div class="is-full is-flex-column" bind:this={element} >
   <div class="is-flexfix backstage" style="height: {topheight};"><slot name="top" /></div>
-  <div class="is-flexfix dragbar" class:dragging on:mousedown={register}>
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" height="22" viewBox="0 0 24 24" width="22"><path d="M3.984 15v-2.016h16.031V15H3.984zm16.032-6v2.016H3.985V9h16.031z"/></svg> -->
-  </div>
+  <div class="is-flexfix dragbar" class:dragging on:mousedown={register}></div>
   <slot name="bottom" />
 </div>
 
@@ -43,11 +41,7 @@
 }
 .dragging,
 .dragbar:hover {
-  /* background-color: var(--c-basic-300); */
   background-color: var(--c-primary);
   transform: scaleY(1.7);
 }
-/* .dragbar svg {
-  fill: var(--c-basic-900);
-} */
 </style>
