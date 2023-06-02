cd0fc7e65e9e601cff9faf651b532e814d21933e
onza <info@martinfarkas.com>
2023-06-02 10:51:10 +0200

    new nav design

--- src/client/components/sidebar/SidebarNav.svelte
+++ src/client/components/sidebar/SidebarNav.svelte
@@ -11,7 +11,7 @@
 
 </script>
 
-<ul class="list list_level-{level}">
+<ul class="list level-{level}">
   {#each nodes as node }
     {#if node.isLeaf}
       <li>
@@ -19,7 +19,7 @@
       </li>
     {:else}
       <li class="folder" class:unfolded={node.unfolded}>
-        <button class="folder_button btn_level-{level}" title={node.unfolded ? 'fold' : 'unfold'} 
+        <button class="folder_btn btn_level-{level}" title={node.unfolded ? 'fold' : 'unfold'} 
           on:click={() => toggleFoldStatusOfNode(node)}>
           <span class="folder_icon" class:unfolded_icon={node.unfolded}><svg xmlns="http://www.w3.org/2000/svg" width="15" viewBox="0 0 24 24"  height="15" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 15l-6-6-6 6"/></svg></span>
           <span class="folder_label">{@html node.name}</span>
@@ -33,76 +33,64 @@
 </ul>
 
 <style>
-.list_level-1 {
+.level-1 {
   padding: 0.5rem 0 1.25rem;
-  /* border-bottom: 1px solid var(--c-basic-300); */
 }
 .folder {
   display: block;
   margin: 0;
   background-color: transparent;
 }
-.folder_button {
+.folder_btn {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   width: 100%;
   margin: 0;
   padding: 0.5rem 0.688rem 0.375rem 0.688rem;
-  text-align: left;
-  color: var(--c-basic-900);
   background-color: transparent;
   cursor: pointer;
 }
 .folder_label {
   display: flex;
-  align-self: center;
-  order: 1;
   width: 100%;
-  color: var(--c-basic-600);
   font-size: 0.813rem;
-  font-weight: 500;
+  color: var(--c-basic-900);
+  font-weight: 600;
   text-transform: uppercase;
 }
-.folder_button:hover .folder_label {
+.folder_btn:hover .folder_label {
   color: var(--c-basic-900);
 }
 .folder_icon {
   display: flex;
-  justify-items: center;
   align-items: center;
-  order: 2;
   width: 0.938rem;
   height: 1.25rem;
   transition: 0.2s;
-  margin-left: 0.25rem;
-  margin-right: 0;
+  margin-right: 0.375rem;
 }
 .folder_icon svg {
-  color: var(--c-basic-700);
+  color: var(--c-basic-900);
 }
 .folder_icon.unfolded_icon {
   transform: rotate(180deg);
   transition: 0.2s;
 }
-.btn_level-1 .folder_icon {
-  margin-left: 0;
-  margin-right: 0.25rem;
-}
-.btn_level-1 .folder_label {
-  order: 2;
-}
-.btn_level-1 .folder_icon {
-  order: 1;
-}
-.btn_level-2 {
-  padding-left: 2rem;
-}
-.btn_level-3 {
-  padding-left: 3rem;
-}
 .btn_level-3 .folder_label {
   font-size: 0.813rem;
+}
+.level-2 {
+  margin-left: 1.063rem;
+  border-left: 1px solid var(--c-basic-300);
+}
+.level-3 {
+  margin-left: 1.063rem;
+  border-left: 1px solid var(--c-basic-300);
+}
+.level-4 {
+  margin-left: 1.063rem;
+  border-left: 1px solid var(--c-basic-300);
 }
 .list :global(.highlight) {
   padding: 0 0.125rem;
--- src/client/components/sidebar/SidebarNavLinks.svelte
+++ src/client/components/sidebar/SidebarNavLinks.svelte
@@ -14,41 +14,27 @@
 </a>
 
 <style>
-  a {
-    padding: 0.188rem 0 0.188rem 0.75rem;
-    text-transform: initial;
-    font-size: 0.813rem;
-    color: var(--c-basic-600);
-    line-height: 1.3;
-    font-weight: 400;
-    border-left: 1px solid var(--c-basic-400);
-  }
-  a:hover,
-  a:focus {
-    color: var(--c-basic-900);
-    border-left: 3px solid var(--c-primary);
-  }
-  a.selected {
-    color: var(--c-primary);
-    border-left: 3px solid var(--c-primary);
-    padding-left: calc(0.75rem - 2px) !important;
-    background-color: var(--c-primary-bg);
-  }
-  a.selected:hover {
-    padding-left: 0.75rem;
-  }
-  .link_level-2 {
-    margin-left: 1.938rem ;
-  }
-  .link_level-3 {
-    margin-left: 2.063rem ;
-  }
-  .link_level-4 {
-    margin-left: 3.063rem ;
-  }
-  .link_level-2:hover,
-  .link_level-3:hover,
-  .link_level-4:hover {
-    padding-left: calc(0.75rem - 2px);
-  }
+a {
+  margin: 0 0 0 -0.125rem;
+  padding: 0.188rem 0 0.188rem 0.813rem;
+  text-transform: initial;
+  font-size: 0.813rem;
+  color: var(--c-basic-600);
+  line-height: 1.3;
+  font-weight: 400;
+  border-left: 0.188rem solid transparent;
+}
+a:hover,
+a:focus {
+  color: var(--c-basic-900);
+  border-color: var(--c-primary);
+}
+a.selected {
+  color: var(--c-primary);
+  border-color: var(--c-primary);
+  background-color: var(--c-primary-bg);
+}
+a.selected:hover {
+  padding-left: 0.75rem;
+}
 </style>
