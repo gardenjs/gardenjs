2dc2d30853abda1d59e1883bc1481208dcd572a1
Robin Danzinger <robin.danzinger@gmx.com>
2023-10-24 16:58:00 +0200

    configure open browser

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import open from 'open'
 import { createServer as createViteServer } from 'vite'
 
 export async function createServer() {
-  const { serverport, vite_config } = await getConfig()
+  const { serverport, vite_config, devmodus, no_open_browser } =
+    await getConfig()
 
   const configFile = vite_config || './vite.config.js'
 
@@ -17,7 +18,12 @@ export async function createServer() {
     },
   })
   console.log(`Listening to port ${serverport}`)
-  console.log(`http://localhost:${serverport}`)
   server.listen()
-  open(`http://localhost:${serverport}`)
+  if (devmodus) {
+    console.log('!!!!RUNNING IN DEVELOPMENT-MODUS!!!!')
+  }
+  console.log('')
+  console.log(`Goto -> http://localhost:${serverport}`)
+  console.log('')
+  if (!no_open_browser) open(`http://localhost:${serverport}`)
 }
