993bd87ca58c9ee87e633f099c9695aadbb32a72
onza <info@martinfarkas.com>
2023-12-05 15:47:27 +0100

    remove

--- ba.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm run build
-cp src/codegenerator/shapes/* ../minesweeper/.garden/ -r
-cp dist/* ../minesweeper/.garden/ -r
-
-cp src/codegenerator/shapes/* ../vu/.garden/ -r
-cp dist/* ../vu/.garden/ -r
