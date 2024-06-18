c1cefd76247960b001dce3c40639a4e71e326f46
onza <info@martinfarkas.com>
2024-06-18 15:36:34 +0200

    format: add prettier-ignore

--- src/client/components/stage/panel/PanelComponent.svelte
+++ src/client/components/stage/panel/PanelComponent.svelte
@@ -38,40 +38,15 @@
         <ul>
           {#each tabs as tab}
             <li>
-              <button
-                class:active={tab == selectedTab}
-                on:click={handleSelect(tab)}
-              >
-                {tab.name}
-                <span class="dot"></span>
-              </button>
+              <!-- prettier-ignore -->
+              <button class:active={tab == selectedTab} on:click={handleSelect(tab)}>{tab.name}<span class="dot"></span></button>
             </li>
           {/each}
         </ul>
       </nav>
-      <button
-        class="panel_toggle"
-        title="Collapse panel"
-        on:click={handleCollapsePanel}
-      >
-        <svg
-          class="controls_btn-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          ><rect width="18" height="18" x="3" y="3" rx="2" ry="2" /><line
-            x1="3"
-            x2="21"
-            y1="15"
-            y2="15"
-          /><path d="m15 8-3 3-3-3" /></svg
-        >
+      <!-- prettier-ignore -->
+      <button class="panel_toggle" title="Collapse panel" on:click={handleCollapsePanel}>
+        <svg class="controls_btn-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="3" rx="2" ry="2" /><line x1="3" x2="21" y1="15" y2="15"/><path d="m15 8-3 3-3-3" /></svg>
       </button>
     </div>
     <div class="panel_pane">
