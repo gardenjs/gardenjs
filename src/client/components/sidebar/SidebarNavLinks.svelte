ac3192c4754d99e648537beeaee0e808ed0dd2b2
onza <info@martinfarkas.com>
2024-09-20 15:23:06 +0200

    remove unused level

--- src/client/components/sidebar/SidebarNavLinks.svelte
+++ src/client/components/sidebar/SidebarNavLinks.svelte
@@ -1,7 +1,6 @@
 <script>
   export let href
   export let selected
-  export let level
 
   function handleClick() {
     globalThis.history.pushState({}, '', href)
@@ -9,7 +8,7 @@
 </script>
 
 <!-- prettier-ignore -->
-<a {href} class="component_link link_level-{level}" class:selected on:click|preventDefault={handleClick}>
+<a {href} class="component_link" class:selected on:click|preventDefault={handleClick}>
   <span class="component_dot"></span>
   <span class="component_icon">
     <svg xmlns="http://www.w3.org/2000/svg" width="15" viewBox="0 0 24 24" height="15" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21 8a2 2 0 00-1-1.73l-7-4a2 2 0 00-2 0l-7 4A2 2 0 003 8v8a2 2 0 001 1.73l7 4a2 2 0 002 0l7-4A2 2 0 0021 16z" /><path d="M3.3 7l8.7 5 8.7-5M12 22V12" /></svg>
@@ -23,7 +22,7 @@
     justify-content: flex-start;
     align-items: center;
     margin: 0;
-    padding: 0.188rem 0 0.188rem 0;
+    padding: 0.188rem 0;
     text-transform: initial;
     font-size: 0.813rem;
     color: var(--c-basic-600);
