0958c597b99199388f0d46ccb4410d461918d067
onza <info@martinfarkas.com>
2025-09-27 00:14:18 +0200

    add styles

--- src/client/components/stage/Inspector.svelte
+++ src/client/components/stage/Inspector.svelte
@@ -5,6 +5,7 @@
   let overlay
   let marginBox
   let paddingBox
+  let contentBox
 
   let margin = $state()
   let padding = $state()
@@ -24,7 +25,12 @@
     content = {
       width: parseFloat(style.width),
       height: parseFloat(style.height),
-      background: style.background,
+      background: style.backgroundColor,
+
+      gap: parseFloat(style.gap),
+      gapUnit: style.gap.replace(/[0-9.]/g, ''),
+      rowGap: parseFloat(style.rowGap),
+      columnGap: parseFloat(style.columnGap),
     }
 
     margin = {
@@ -63,6 +69,11 @@
     paddingBox.style.left = margin.left + 'px'
     paddingBox.style.width = rect.width + 'px'
     paddingBox.style.height = rect.height + 'px'
+
+    contentBox.style.top = margin.top + padding.top + 'px'
+    contentBox.style.left = margin.left + padding.left + 'px'
+    contentBox.style.width = rect.width + -padding.left - padding.right + 'px'
+    contentBox.style.height = rect.height + -padding.top - padding.bottom + 'px'
 
     infobox.style.top =
       overlayBottom + 200 < contentPane.offsetHeight
@@ -103,30 +114,40 @@
   <div class="boxes">
     <div class="marginBox" bind:this={marginBox} />
     <div class="paddingBox" bind:this={paddingBox} />
+    <div class="contentBox" bind:this={contentBox} />
   </div>
 </div>
 {#if padding && margin}
   <div class="infobox" bind:this={infobox}>
-    <div>
-      width: {content.width}
+    <div class="info-item">
+      <div class="attribute">Width:</div>
+      <div class="value">{content.width}px</div>
     </div>
-    <div>
-      height: {content.height}
+    <div class="info-item">
+      <div class="attribute">Height:</div>
+      <div class="value">{content.height}px</div>
     </div>
-    <div>
-      background: {content.background}
+    <!-- <div class="info-item">
+      <div class="attribute">Gap:</div>
+      <div class="value">{content.gap} {content.gapUnit} {content.columnGap} {content.rowGap}</div>
+    </div> -->
+    <div class="info-item">
+      <div class="attribute">Margin:</div>
+      <div class="value">
+        {margin.top}{margin.top !== 0 ? 'px' : ''}
+        {margin.right}{margin.right !== 0 ? 'px' : ''}
+        {margin.bottom}{margin.bottom !== 0 ? 'px' : ''}
+        {margin.left}{margin.left !== 0 ? 'px' : ''}
+      </div>
     </div>
-    <div>
-      margin: {margin.top}
-      {margin.right}
-      {margin.bottom}
-      {margin.left}
-    </div>
-    <div>
-      padding: {padding.top}
-      {padding.right}
-      {padding.bottom}
-      {padding.left}
+    <div class="info-item">
+      <div class="attribute">Padding:</div>
+      <div class="value">
+        {padding.top}{margin.top !== 0 ? 'px' : ''}
+        {padding.right}{padding.right !== 0 ? 'px' : ''}
+        {padding.bottom}{padding.bottom !== 0 ? 'px' : ''}
+        {padding.left}{padding.left !== 0 ? 'px' : ''}
+      </div>
     </div>
   </div>
 {/if}
@@ -137,26 +158,73 @@
     position: absolute;
     z-index: 999999;
     pointer-events: none;
-    border: 2px solid rgba(0, 150, 255, 0.8);
   }
   .boxes {
     width: 100%;
     height: 100%;
   }
+  .contentBox {
+    position: absolute;
+    background: hsla(210, 75%, 50%, 0.45);
+    overflow: hidden;
+  }
   .marginBox {
     position: absolute;
-    background: rgba(255, 165, 0, 0.3);
+    background: hsla(30, 80%, 50%, 0.45);
+    overflow: hidden;
   }
   .paddingBox {
     position: absolute;
-    background: rgba(0, 255, 0, 0.3);
+    background: hsla(120, 50%, 70%, 0.45);
+    overflow: hidden;
   }
+  /* .gapBox {
+    position: absolute;
+    background: hsla(256, 55%, 55%, 0.45);
+    overflow: hidden;
+  } */
   .infobox {
-    margin-top: 10px;
+    padding: 0.5rem;
     position: absolute;
     z-index: 999999;
-    background-color: gray;
-    opacity: 0.8;
-    width: 360px;
+    background-color: #fff;
+    border-radius: 0.5rem;
+    filter: drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.05))
+      drop-shadow(0 1px 3px rgba(0, 0, 0, 0.1));
+    font-variation-settings: normal;
+    font-family:
+      system-ui,
+      -apple-system,
+      'Segoe UI',
+      Roboto,
+      Helvetica,
+      Arial,
+      sans-serif,
+      'Apple Color Emoji',
+      'Segoe UI Emoji';
+    font-size: 0.75rem;
+    color: #000;
+    letter-spacing: 0.5px;
+    line-height: 1.6;
+    overflow: visible;
+  }
+  .infobox::after {
+    content: '';
+    position: absolute;
+    width: 1.75rem;
+    height: 1.125rem;
+    background: #fff;
+    clip-path: polygon(0 0, 100% 0, 50% 100%); /* pointy at the bottom */
+    filter: drop-shadow(0px 5px 5px rgba(0, 0, 0, 0.05))
+      drop-shadow(0 1px 3px rgba(0, 0, 0, 0.1));
+    left: 1rem;
+  }
+  .info-item {
+    display: flex;
+    flex-direction: row;
+  }
+  .attribute {
+    width: 8rem;
+    /* font-weight: 600; */
   }
 </style>
